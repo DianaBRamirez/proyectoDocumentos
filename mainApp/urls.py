571ec6c57060a9a93fc47090be5dcb0e31ded2cd
DianaBRamirez <dianabramirez29@gmail.com>
2024-06-08 18:42:40 -0600

    cambios

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,13 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    # path('atencion', views.obtenerCursos),
-    # path('registrarCurso/', views.registrarCurso),
-    # path('edicionCurso/<codigo>', views.edicionCurso),
-    # path('editarCurso/', views.editarCurso),
-    # path('eliminarCurso/<id>', views.eliminarCurso),
-    # path('signin/', views.signin, name='signin'),
-    # path('signup/', views.signup, name='signup'),
-    # path('logout/', views.signout, name='logout'),
-  #   path('pruebaCurso/<codigo>', views.pruebaCurso)
+    path('agregarActividad', views.agregarActividadTutorial, name='agregarActividad'),
+
 ]
